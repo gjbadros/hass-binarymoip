27a094d4fd11ee1af16cbe56c2e84dee6566ea48
Greg Badros <badros@gmail.com>
2019-08-13 17:17:38 -0700

    Not sure what select_source passes.

--- media_player.py
+++ media_player.py
@@ -63,7 +63,8 @@ class MoIP_MediaPlayer_Rx(MediaPlayerDevice):
 
     def select_source(self, source_num):
         """Select input source."""
-        self._rx.switch_to_tx(source_num)
+        _LOGGER.info("Switching to %s", source_num)
+        self._rx.switch_to_tx(int(source_num))
         self.schedule_update_ha_state()
 
     @property
